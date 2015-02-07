d15fb182541f92e0673a9ecdeaedda1144c5ed61
Brandon Sprague <brandon00sprague@gmail.com>
2015-02-07 18:56:31 -0500

    Sign Language killing it, still have no idea what I'm doing
    Literally the one fancy thing I tried made my score worse, but I'm in
    first, so lololol trololol

--- Sign-Language/code.py
+++ Sign-Language/code.py
@@ -3,73 +3,27 @@ import operator
 
 import numpy as np
 from sklearn import cross_validation
-from sklearn import tree
-from sklearn import naive_bayes
-from sklearn import ensemble
-from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.neighbors import KNeighborsClassifier
-
-# I stole this gem of a function from: http://stackoverflow.com/questions/1518522/python-most-common-element-in-a-list
-def most_common(L):
-  # get an iterable of (item, iterable) pairs
-  SL = sorted((x, i) for i, x in enumerate(L))
-  groups = itertools.groupby(SL, key=operator.itemgetter(0))
-  def _auxfun(g):
-    item, iterable = g
-    count = 0
-    min_index = len(L)
-    for _, where in iterable:
-      count += 1
-      min_index = min(min_index, where)
-    return count, -min_index
-  # pick the highest-count/earliest item
-  return max(groups, key=_auxfun)[0]
-
-# Now I rock out some ensemble-ception, because hopefully these are all
-# wrong in different ways, and I choose the most common answer. If they're all
-# different let's go with the first, and I'll order my classifiers based on
-# which one is my favorite
-def predictificate(data, target, test, clfs):
-    res = []
-    for clf in clfs:
-        clf.fit(data, target)
-        res.append(clf.predict(test))
-
-    pred = [most_common(x) for x in zip(*res)]
-    f = open('predictions.csv', 'w')
-    f.write("ID,Category\n")
-
-    for i, res in enumerate(pred):
-        f.write("%d,%d\n" % (i+1,res))
-
-    f.close()
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 
 train = np.load('train.npy')
 # Remove the labels
-#test = np.load('test_distribute.npy')[:,1:]
+test = np.load('test_distribute.npy')[:,1:]
 
-data = train[:,1:12]
+data = train[:,1:]
 target = train[:,0]
 
-clfs = []
-# Through cv testing, I found optimal C to be 0.0001
-clfs.append(naive_bayes.MultinomialNB(alpha=10**-5))
+# Originally, I thought removing these fields would be good, but I actually do better by not removing them, tragic
+#data = np.delete(data, np.s_[[x for x in range(990) if x % 15 in [3,4,5,10,11,12,13,14]]], 1)
+#test = np.delete(test, np.s_[[x for x in range(990) if x % 15 in [3,4,5,10,11,12,13,14]]], 1)
 
-# Through cv testing, I found optimal depth to be 11
-clfs.append(tree.DecisionTreeClassifier(max_depth=11))
+#clf = svm.LinearSVC()
+pred = RandomForestClassifier(n_estimators=100).fit(data, target).predict(test)
 
-# Through cv testing, I found the optimal number of estimators to be 15
-clfs.append(ensemble.RandomForestClassifier(n_estimators=40))
+f = open('predictions.csv', 'w')
+f.write("ID,Category\n")
 
-# You know the deal, for the next two, messy testing to get hyperparameters
-clfs.append(BaggingClassifier(KNeighborsClassifier(),max_samples=0.6, max_features=0.3))
-clfs.append(AdaBoostClassifier(n_estimators=40))
+for i, res in enumerate(pred):
+    f.write("%d,%d\n" % (i+1,res))
 
-#predictificate(data, target, test, clfs)
-
-# I use the following code to find good hyperparameter values
-clf = tree.DecisionTreeClassifier()
-scores = cross_validation.cross_val_score(
-    clf, data, target, cv=5)
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+f.close()
