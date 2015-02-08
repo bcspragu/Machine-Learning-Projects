f8909a4485484d2cf19e7dfec44d656902f1c2eb
Brandon Sprague <brandon00sprague@gmail.com>
2015-02-07 20:02:32 -0500

    Can't even remember what I changed

--- Beats/code.py
+++ Beats/code.py
@@ -4,7 +4,7 @@ import operator
 import numpy as np
 from sklearn import cross_validation
 from sklearn import feature_selection
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import ensemble
 
 train = np.load('train.npy')
 # Remove the labels
@@ -13,19 +13,53 @@ test = np.load('test_distribute.npy')[:,1:]
 data = train[:,1:]
 target = train[:,0]
 
+def most_common(L):
+  # get an iterable of (item, iterable) pairs
+  SL = sorted((x, i) for i, x in enumerate(L))
+  groups = itertools.groupby(SL, key=operator.itemgetter(0))
+  def _auxfun(g):
+    item, iterable = g
+    count = 0
+    min_index = len(L)
+    for _, where in iterable:
+      count += 1
+      min_index = min(min_index, where)
+    return count, -min_index
+  # pick the highest-count/earliest item
+  return max(groups, key=_auxfun)[0]
+
+# Now I rock out some ensemble-ception, because hopefully these are all
+# wrong in different ways, and I choose the most common answer. If they're all
+# different let's go with the first, and I'll order my classifiers based on
+# which one is my favorite
+def predictificate(data, target, test, clfs):
+    res = []
+    for clf in clfs:
+        clf.fit(data, target)
+        res.append(clf.predict(test))
+
+    pred = [most_common(x) for x in zip(*res)]
+    f = open('predictions.csv', 'w')
+    f.write("ID,Category\n")
+
+    for i, res in enumerate(pred):
+        f.write("%d,%d\n" % (i+1,res))
+
+    f.close()
+
 trimmer = feature_selection.SelectPercentile(percentile=15).fit(data, target)
 data = trimmer.transform(data)
 test = trimmer.transform(test)
 
-pred = RandomForestClassifier(n_estimators=100).fit(data, target).predict(test)
+clfs = []
 
-f = open('predictions.csv', 'w')
-f.write("ID,Category\n")
+# Through cv testing, I found the optimal number of estimators to be 15
+clfs.append(ensemble.RandomForestClassifier(n_estimators=150))
+clfs.append(ensemble.GradientBoostingClassifier(n_estimators=200))
+clfs.append(ensemble.AdaBoostClassifier(n_estimators=135))
 
-for i, res in enumerate(pred):
-    f.write("%d,%d\n" % (i+1,res))
+predictificate(data, target, test, clfs)
 
-f.close()
 # I use the following code to find good hyperparameter values
 #scores = cross_validation.cross_val_score(
     #clf, data, target, cv=5)
--- Black-Box/code.py
+++ Black-Box/code.py
@@ -24,7 +24,7 @@ trimmer = SelectKBest(chi2, k=400).fit(data, target)
 trimmed_data = trimmer.transform(data)
 trimmed_test = trimmer.transform(test)
 
-clf = ensemble.RandomForestClassifier(n_estimators=50)
+clf = ensemble.RandomForestClassifier(n_estimators=100)
 pred = clf.fit(trimmed_data, target).predict(trimmed_test)
 
 f = open('predictions.csv', 'w')
@@ -39,4 +39,4 @@ f.close()
 # I use the following code to find good hyperparameter values
 #scores = cross_validation.cross_val_score(
     #clf, trimmed_data, target, cv=5)
-#print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+#print("Accuracy: %f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
